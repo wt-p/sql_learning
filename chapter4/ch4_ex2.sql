822e6b55dc2d7141f5cb0195727192e063bbe7f1
wt-p <wtbtty9955@gmail.com>
2025-01-30 22:41:45 +0900

    4章の練習問題実施

---
+++ chapter4/ch4_ex2.sql
@@ -0,0 +1,18 @@
+/* 問題4-2 */
+
+-- 1
+SELECT 値 FROM 奇数
+UNION
+SELECT 値 FROM 偶数;
+-- 2
+SELECT 値 FROM 整数
+EXCEPT
+SELECt 値 FROM 偶数;
+-- 3
+SELECT 値 FROM 整数
+INTERSECT
+SELECT 値 FROM 偶数;
+-- 4
+SELECT 値 FROM 奇数
+INTERSECT
+SELECT 値 FROM 偶数;
